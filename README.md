e276dc4ad13bd7da4b650d62c53ccfc1180f1bf8
smjuni <stephaniejuniper@hotmail.com>
2021-05-16 20:24:59 -0700

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NZ_Case_Study
-noissue case study
+e-commerce case study
+
+<a href="https://docs.google.com/presentation/d/1DcW7QfniRAjxMOApd4tarjapJdheeMOt1_BontmUs4c/edit#slide=id.gc6f73a04f_0_20"> Google Slides Presentation </a>
+
+<a href="https://public.tableau.com/profile/stephanie.m.juniper#!/vizhome/NZCaseStudy/OrdersbyCustomerType?publish=yes">Tableau Public Link</a>
---
+++ queries.sql
@@ -0,0 +1,31 @@
+SELECT * 
+FROM sales_data;
+
+-- Show Top Three SKUS by ORDERS for the most recent 8 weeks
+SELECT item_sku,
+SUM(skus_count) AS "Number Ordered"
+FROM sales_data
+WHERE order_date BETWEEN '2020-08-05' AND '2020-09-30'
+GROUP BY item_sku
+ORDER BY "Number Ordered" DESC
+LIMIT 3;
+
+
+-- AVERAGE ORDER VALUE BY PRODUCT
+SELECT product_category,
+AVG(sales) AS "Average Order Value"
+FROM sales_data
+GROUP BY product_category
+ORDER BY AVG(sales) DESC;
+
+
+--Product Category with the highest repeat rate
+SELECT product_category,
+COUNT(new_or_repeat_by_order='Repeat') AS "Repeat Orders"
+FROM sales_data
+WHERE new_or_repeat_by_order ='Repeat'
+GROUP BY product_category
+ORDER BY "Repeat Orders" DESC
+LIMIT 1;
+
+Noissue Tissue
